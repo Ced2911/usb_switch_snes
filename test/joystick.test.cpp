229a6c88514bebfa90df3ef8eb87d32ed152ab4e
Ced2911 <ced2911@gmail.com>
2019-05-23 00:00:31 +0200

    test

--- test/joystick.test.cpp
+++ test/joystick.test.cpp
@@ -3,25 +3,19 @@
 #include "joystick.h"
 #include <unistd.h>
 
-void test_struct() {
-    JoystickInput_t joystickInput = {
-        .report_id = 0x30
-    };
-    uint8_t * data = (uint8_t *)&joystickInput;
-    TEST_ASSERT_EQUAL(0x30, joystickInput.report_id);
-    TEST_ASSERT_EQUAL(0x30, data[0]);
+
+size_t test1() {
+  return sizeof(struct JoystickInput_t);
 }
 
 
 SCENARIO("Test struct") {
+    
 
   GIVEN("a one second sleep duration") {
-    JoystickInput_t joystickInput = {
-        .report_id = 0x30
-    };
     WHEN("call sleep with this duration") {
       THEN("it's expected nobody interupted our rest") {
-        CHECK(sizeof(JoystickInput_t) == 63);
+        CHECK(test1() == 0x39)
       }
     }
   }
