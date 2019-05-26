bf52efa293a5298a2817cb768dacd1e58d5cd351
Ced2911 <ced2911@gmail.com>
2019-05-26 09:01:42 +0200

    plop

--- src/usart.c
+++ src/usart.c
@@ -4,24 +4,31 @@
 
 void usart_init()
 {
-    rcc_periph_clock_enable(RCC_USART1);
+    rcc_periph_clock_enable(RCC_AFIO);
+    rcc_periph_clock_enable(RCC_USART2);
 
     /* Setup GPIO pin GPIO_USART1_TX. */
     gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
+                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
 
     /* Setup UART parameters. */
-    usart_set_baudrate(USART1, 38400);
-    usart_set_databits(USART1, 8);
-    usart_set_stopbits(USART1, USART_STOPBITS_1);
-    usart_set_mode(USART1, USART_MODE_TX);
-    usart_set_parity(USART1, USART_PARITY_NONE);
-    usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+    usart_set_baudrate(USART2, 38400);
+    usart_set_databits(USART2, 8);
+    usart_set_stopbits(USART2, USART_STOPBITS_1);
+    usart_set_mode(USART2, USART_MODE_TX);
+    usart_set_parity(USART2, USART_PARITY_NONE);
+    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+
+    usart_enable(USART2);
 }
 
 void usart_send_str(char *p)
 {
     char *ptr = p;
-    while (*ptr++ != 0)
-        usart_send_blocking(USART1, *ptr);
+    do
+    {
+        usart_send_blocking(USART2, *ptr);
+    } while (*ptr++ != 0);
+
+    usart_send_blocking(USART2, '\n');
 }
--- test/joystick.test.cpp
+++ test/joystick.test.cpp
@@ -4,19 +4,46 @@
 #include <unistd.h>
 
 
-size_t test1() {
-  return sizeof(struct JoystickInput_t);
-}
-
 
 SCENARIO("Test struct") {
     
 
   GIVEN("a one second sleep duration") {
-    WHEN("call sleep with this duration") {
-      THEN("it's expected nobody interupted our rest") {
-        CHECK(test1() == 0x39)
-      }
+    WHEN("check ControllerDataReport") {
+        CHECK(sizeof(struct ControllerDataReport) == 0x39);
+    }
+    WHEN("check SpiReadReport") {
+        CHECK(sizeof(struct SpiReadReport) == 0x39);
+    }
+
+    WHEN("check Report81Response") {
+        CHECK(sizeof(struct Report81Response) == 0x39);
+    }
+
+
+    WHEN("check output_report_0x01_get_device_info ") {
+        uint8_t usb_out_buf[0x40] =  {0};
+        struct Report81Response *resp = (struct Report81Response *)&usb_out_buf[0x01];
+        usb_out_buf[0x00] = kUsbReportIdInput81;
+
+        resp->subcommand_ack = 0x82;
+        resp->subcommand = 0x02;
+        resp->cmd_0x02.firmware_version = 0x4803;
+        resp->cmd_0x02.device_type = 0x03;
+        resp->cmd_0x02.unk_0 = 0x02;
+
+        // mac address
+        // for (int i = 0; i < 6; i++)
+        //    resp->cmd_0x02.mac[i] = i;
+
+        const uint8_t response_h[] = {0x57, 0x30, 0xea, 0x8a, 0xbb, 0x7c};
+        memcpy(resp->cmd_0x02.mac, response_h, sizeof(response_h));
+
+        resp->cmd_0x02.unk_1 = 0x01;
+        resp->cmd_0x02.use_spi_colors = 0x01;
+
+
+        CHECK( (*(uint16_t *)&usb_out_buf[0xD] == 0x0282));
     }
   }
 } 
