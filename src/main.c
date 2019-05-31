5e51eb103b59b2fc3ebd9d5625303303c40aaa4e
Ced2911 <ced2911@gmail.com>
2019-05-31 21:39:16 +0200

    plop

--- src/main.c
+++ src/main.c
@@ -35,7 +35,7 @@ void dump_hex(const void *data, size_t size)
         //*ptr++ = (HEX_CHAR((b >> 4) & 0xf));
         //*ptr++ = (HEX_CHAR(b & 0xf));
         //*ptr++ = ' ';
-        ptr += sprintf(ptr, "0x%02x, ", b);
+        ptr += sprintf(ptr, "0x%x,", b);
     }
 
     *ptr++ = '\r';
@@ -46,7 +46,7 @@ void dump_hex(const void *data, size_t size)
     //usb_poll();
     usart_send_str(ascii_buffer);
 }
-#define dump_hex(...)
+//#define dump_hex(...)
 
 #ifndef TEST
 static mutex_t m;
@@ -133,7 +133,7 @@ void fill_input_report(struct ControllerData *controller_data)
 
     controller_data->battery_level = /*battery_level_charging | */ battery_level_full;
     controller_data->connection_info = 0x1;
-    controller_data->vibrator_input_report = 0xB0;
+    controller_data->vibrator_input_report = 0x80;
 }
 
 // Standard full mode - input reports with IMU data instead of subcommand replies. Pushes current state @60Hz, or @120Hz if Pro Controller.
@@ -340,39 +340,15 @@ void output_report_0x01_0x08_lowpower_state(uint8_t *buf, uint8_t *usb_out_buf)
 void output_report_0x01_get_device_info(uint8_t *buf, uint8_t *usb_out_buf)
 {
     usart_send_str(__func__);
-#if 0
+    unsigned char rawData[64] = {
+        0x21, 0x05, 0x8E, 0x84, 0x00, 0x12, 0x01, 0x18, 0x80, 0x01, 0x18, 0x80, 0x80,
+        0x82, 0x02, 0x03, 0x48, 0x01, kUsbDeviceTypeProController, mac_addr[5], mac_addr[4], mac_addr[3], mac_addr[2], mac_addr[1], mac_addr[0], 0x01, 0x01};
+
+    memcpy(usb_out_buf, rawData, 0x40);
     struct Report81Response *resp = (struct Report81Response *)&usb_out_buf[0x01];
-    usb_out_buf[0x00] = kUsbReportIdInput81;
-
-    resp->subcommand_ack = 0x82;
-    resp->subcommand = 0x02;
-    resp->cmd_0x02.firmware_version = 0x4803;
-    resp->cmd_0x02.device_type = 0x03;
-    resp->cmd_0x02.unk_0 = 0x02;
-
-    // mac address
-    // for (int i = 0; i < 6; i++)
-    //    resp->cmd_0x02.mac[i] = i;
-
-    const uint8_t response_h[] = {0x7c, 0xbb, 0x8a, 0xea, 0x30, 0x57 };
-    memcpy(resp->cmd_0x02.mac, response_h, sizeof(response_h));
-
-    resp->cmd_0x02.unk_1 = 0x01;
-    resp->cmd_0x02.use_spi_colors = 0x01;
-
-    
     fill_input_report(&resp->controller_data);
 
     usb_write_packet(ENDPOINT_HID_IN, usb_out_buf, 0x40);
-#else
-    unsigned char rawData[64] = {
-        0x21, 0x05, 0x8E, 0x84, 0x00, 0x12, 0x01, 0x18, 0x80, 0x01, 0x18, 0x80, 0x80,
-        0x82, 0x02, 0x03, 0x48, 0x01, 0x02, mac_addr[5], mac_addr[4], mac_addr[3], mac_addr[2], mac_addr[1], mac_addr[0], 0x01, 0x01};
-    static int iii = 0;
-    rawData[0x01] = iii++;
-    memcpy(usb_out_buf, rawData, 0x40);
-    usb_write_packet(ENDPOINT_HID_IN, usb_out_buf, 0x40);
-#endif
 }
 
 // Subcommand 0x03: Set input report mode
@@ -525,6 +501,10 @@ void output_report_0x01_set_vibration(uint8_t *buf, uint8_t *usb_out_buf)
     struct ResponseX81 *resp = (struct ResponseX81 *)&usb_out_buf[0x01];
     // report ID
     usb_out_buf[0x00] = kReportIdInput21;
+
+    resp->subcommand_ack = 0x80;
+    resp->subcommand = 0x48;
+
     fill_input_report(&resp->controller_data);
     usb_write_packet(ENDPOINT_HID_IN, usb_out_buf, 0x40);
 }
@@ -622,6 +602,8 @@ void output_report_0x10(uint8_t *buf, uint8_t *usb_out_buf)
     /** nothing **/
     // Joy-con does not reply when Output Report is 0x10
     usart_send_str(__func__);
+
+    // output_report_0x01_unknown_subcmd(buf, usb_out_buf);
 }
 
 // Sub command !
@@ -701,7 +683,7 @@ void do_work(uint8_t *current_usb_buf, uint8_t len)
     uint8_t usb_out_buf[0x40];
 
     usart_send_str("Recv: ");
-    dump_hex(current_usb_buf, 0x40);
+    dump_hex(current_usb_buf, 0x10);
 
     switch (cmd)
     {
@@ -724,8 +706,8 @@ void do_work(uint8_t *current_usb_buf, uint8_t len)
 
     if (kReportIdOutput10 != cmd)
     {
-        usart_send_str("Response: ");
-        dump_hex(usb_out_buf, 0x40);
+        //usart_send_str("Response: ");
+        //dump_hex(usb_out_buf, 0x10);
     }
     usart_send_str(" ===== ");
 }
